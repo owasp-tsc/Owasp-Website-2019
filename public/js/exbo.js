fecb560aa6feb66e9c7e7da9f3ff1804ee98d0d9
rachit2501 <rac.sri25@gmail.com>
2019-11-01 00:05:23 +0530

    responsive compressed

---
+++ public/js/exbo.js
@@ -0,0 +1,20 @@
+
+if($(document).height() > $(document).width())
+{   
+    $('.harkirat').click(()=>{
+        $('.harkirat').css("display", "none");
+        $('.gursimran').css("display", "block");
+    })
+    $('.gursimran').click(()=>{
+        $('.gursimran').css("display", "none");
+        $('.sirish').css("display", "block");
+    })
+    $('.sirish').click(()=>{
+        $('.sirish').css("display", "none");
+        $('.lieza').css("display", "block");
+    })
+    $('.lieza').click(()=>{
+        $('.lieza').css("display", "none");
+        $('.harkirat').css("display", "block");
+    })
+}
