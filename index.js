295bb81f858809b97bad0034a00f689fa3a96b52
rachit2501 <rac.sri25@gmail.com>
2019-10-31 17:05:54 +0530

    express routing done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req , res)=>{
+    res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/team' , (req , res)=>{
+    res.sendFile(__dirname + '/team.html');
+});
+
+
+const port = process.env.PORT || 2000;
+
+app.listen(port , ()=>console.log("server up"));
