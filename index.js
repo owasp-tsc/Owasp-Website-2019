278f874e045f15c5f3195b4baebfd7ad831ef4cf
rachit2501 <rac.sri25@gmail.com>
2019-11-07 17:55:31 +0530

    techniti

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-
+app.use(express.static('techniti'));
 app.get('/', (req , res)=>{
     res.sendFile(__dirname + '/home.html');
 });
@@ -10,6 +10,9 @@ app.get('/', (req , res)=>{
 app.get('/team' , (req , res)=>{
     res.sendFile(__dirname + '/team.html');
 });
+app.get('/techniti' , (req , res)=>{
+    res.sendFile(__dirname + '/techniti.html');
+})
 
 
 const port = process.env.PORT || 2000;
